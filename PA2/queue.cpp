8a5a50898341e655db5e40525ba11b4be948b69d
CCWebb14 <CCWebb14@gmail.com>
2024-03-11 22:02:49 -0700

    stack and queue implemented, tests passing

--- PA2/queue.cpp
+++ PA2/queue.cpp
@@ -1,3 +1,4 @@
+
 /**
  * @file        queue.cpp
  * @description Implementation of the Queue class.
@@ -12,11 +13,9 @@
  *
  * @param item - object to be added to the Queue.
  */
-template <class T>
-void Queue<T>::Enqueue(T const& item)
-{
-	// complete your implementation below
-	
+template <class T> void Queue<T>::Enqueue(T const &item) {
+  // complete your implementation below
+  stack_1.Push(item);
 }
 
 /**
@@ -28,13 +27,18 @@ void Queue<T>::Enqueue(T const& item)
  *
  * @return the item that used to be at the front of the Queue.
  */
-template <class T>
-T Queue<T>::Dequeue()
-{
-	// complete your implementation below
-  
-	T item;      // REPLACE THESE LINES
-	return item; // REPLACE THESE LINES
+template <class T> T Queue<T>::Dequeue() {
+  // complete your implementation below
+  // while(!stack_1.IsEmpty()) {
+  //     stack_2.push(stack_1.pop());
+  // }
+  if (stack_2.IsEmpty()) {
+    while (!stack_1.IsEmpty()) {
+      stack_2.Push(stack_1.Pop());
+    }
+  }
+
+  return stack_2.Pop();
 }
 
 /**
@@ -42,65 +46,59 @@ T Queue<T>::Dequeue()
  *
  * See OrderingStructure::Add()
  */
-template <class T>
-void Queue<T>::Add(const T& item)
-{
-	// complete your implementation below
-	// Hint: this function should call a Queue
-	//  function to add the element to the Queue.
-
-	
+template <class T> void Queue<T>::Add(const T &item) {
+  // complete your implementation below
+  // Hint: this function should call a Queue
+  //  function to add the element to the Queue.
+  Enqueue(item);
 }
 
 /*
-*  Removes an element from the ordering structure.
-*
-*  See OrderingStructure::Remove()
-*/
-template <class T>
-T Queue<T>::Remove()
-{
-	// complete your implementation below
-	// Hint: this function should call a Queue
-	// function to remove an element from the Queue and return it. You will
-	// need to replace the following lines.
-  
-	T item;      // REPLACE THESE LINES
-	return item; // REPLACE THESE LINES
+ *  Removes an element from the ordering structure.
+ *
+ *  See OrderingStructure::Remove()
+ */
+template <class T> T Queue<T>::Remove() {
+  // complete your implementation below
+  // Hint: this function should call a Queue
+  // function to remove an element from the Queue and return it. You will
+  // need to replace the following lines.
+
+  return Dequeue();
 }
 
 /*
-*  Finds the object at the front of the Queue, and returns it to
-*  the caller. Unlike Dequeue(), this operation does (conceptually!) not alter the
-*  Queue; however, you may need to alter the internal representation of the Queue.
-*  You may assume that this function is only called when the
-*  Queue is non-empty.
-*
-*  NOTE: This function should have O(1) behavior over n operations!
-*
-*  @return the item at the front of the queue.
-*/
-template <class T>
-T Queue<T>::Peek()
-{
-	// complete your implementation below
-  
-	T item;      // REPLACE THESE LINES
-	return item; // REPLACE THESE LINES
+ *  Finds the object at the front of the Queue, and returns it to
+ *  the caller. Unlike Dequeue(), this operation does (conceptually!) not alter
+ * the Queue; however, you may need to alter the internal representation of the
+ * Queue. You may assume that this function is only called when the Queue is
+ * non-empty.
+ *
+ *  NOTE: This function should have O(1) behavior over n operations!
+ *
+ *  @return the item at the front of the queue.
+ */
+template <class T> T Queue<T>::Peek() {
+  // complete your implementation below
+
+  if (stack_2.IsEmpty()) {
+    while (!stack_1.IsEmpty()) {
+      stack_2.Push(stack_1.Pop());
+    }
+  }
+
+  return stack_2.Peek();
 }
 
 /*
-*  Determines if the Queue is empty.
-*
-*  @return true if the stack is empty,
-*          false otherwise.
-* 
-*  NOTE: This function must always run in O(1) time!
-*/
-template <class T>
-bool Queue<T>::IsEmpty() const
-{
-	// complete your implementation below
-  
-	return true; // REPLACE THIS STUB
+ *  Determines if the Queue is empty.
+ *
+ *  @return true if the stack is empty,
+ *          false otherwise.
+ *
+ *  NOTE: This function must always run in O(1) time!
+ */
+template <class T> bool Queue<T>::IsEmpty() const {
+  // complete your implementation below
+  return (stack_2.IsEmpty() && stack_1.IsEmpty());
 }
