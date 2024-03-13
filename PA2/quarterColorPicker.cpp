0fd2b43666ca2d052b1c47a6e1f60ba5b16afa80
CCWebb14 <CCWebb14@gmail.com>
2024-03-12 23:39:31 -0700

    quarterColorPicker implemented

--- PA2/quarterColorPicker.cpp
+++ PA2/quarterColorPicker.cpp
@@ -8,10 +8,10 @@
 
 #include "quarterColorPicker.h"
 
-QuarterColorPicker::QuarterColorPicker(PNG& inputimg, unsigned char b_amount)
-{
+QuarterColorPicker::QuarterColorPicker(PNG &inputimg, unsigned char b_amount) {
     // Complete your implementation below
-	
+    referenceimg = inputimg;
+    brightamount = b_amount;
 }
 
 /**
@@ -21,19 +21,53 @@ QuarterColorPicker::QuarterColorPicker(PNG& inputimg, unsigned char b_amount)
  * and tile the smaller image in a 2 x 2 grid over the original dimensions
  * of the image, and return the pixel at the appropriate coordinate from
  * the tiled image, brightened on each R/G/B channel by the required amount.
- * 
+ *
  * The value of each pixel in the scaled image will be a bilinear interpolation
  * of a 2x2 pixel region from the original image, with each of the R/G/B/A channels
  * processed individually. Interpolate over the x-axis before the y-axis.
  * Truncate any fractional values on the R/G/B channels. Brighten each color channel
  * as the final step.
- * 
+ *
  * @pre referenceimg width and height are both even
  */
-RGBAPixel QuarterColorPicker::operator()(PixelPoint p)
-{
+RGBAPixel QuarterColorPicker::operator()(PixelPoint p) {
     // Replace the line below with your implementation
-    return RGBAPixel();
+    int true_x;
+    int true_y;
+    if (p.x < referenceimg.width() / 2 && p.y < referenceimg.height() / 2) {
+        // top left
+        true_x = p.x * 2;
+        true_y = p.y * 2;
+    } else if (p.x >= referenceimg.width() / 2 && p.y < referenceimg.height() / 2) {
+        // top right
+        true_x = (p.x - (referenceimg.width() / 2)) * 2;
+        true_y = p.y * 2;
+    } else if (p.x < referenceimg.width() / 2 && p.y >= referenceimg.height() / 2) {
+        // bottom left
+        true_x = p.x * 2;
+        true_y = (p.y - (referenceimg.height() / 2)) * 2;
+    } else {
+        // bottom right
+        true_x = (p.x - (referenceimg.width() / 2)) * 2;
+        true_y = (p.y - (referenceimg.height() / 2)) * 2;
+    }
+
+    RGBAPixel *p1 = referenceimg.getPixel(true_x, true_y);
+    RGBAPixel *p2 = referenceimg.getPixel(true_x + 1, true_y);
+    RGBAPixel *p3 = referenceimg.getPixel(true_x, true_y + 1);
+    RGBAPixel *p4 = referenceimg.getPixel(true_x + 1, true_y + 1);
+    RGBAPixel resPix;
+
+    resPix.r = static_cast<unsigned char>(((p1->r + p2->r) / 2 + (p3->r + p4->r) / 2) / 2);
+    resPix.g = static_cast<unsigned char>(((p1->g + p2->g) / 2 + (p3->g + p4->g) / 2) / 2);
+    resPix.b = static_cast<unsigned char>(((p1->b + p2->b) / 2 + (p3->b + p4->b) / 2) / 2);
+    resPix.a = (((p1->a + p2->a) / 2 + (p3->a + p4->a) / 2) / 2);
+
+    resPix.r = static_cast<unsigned char>(min((resPix.r) + brightamount, 255));
+    resPix.g = static_cast<unsigned char>(min((resPix.g) + brightamount, 255));
+    resPix.b = static_cast<unsigned char>(min((resPix.b) + brightamount, 255));
+
+    return resPix;
 }
 
 /**
--- PA2/quarterColorPicker.h
+++ PA2/quarterColorPicker.h
@@ -8,15 +8,14 @@
 
 #include "colorPicker.h"
 
- /**
-  * QuarterColorPicker: a functor that determines the color that should be
-  * used given an x and y coordinate using a quartered image pattern (the original image
-  * is scaled down and tiled over itself). You can create
-  * private helper functions inside this class, as well as local storage, if
-  * necessary. Remember to overload a destructor if you need to.
-  */
-class QuarterColorPicker : public ColorPicker
-{
+/**
+ * QuarterColorPicker: a functor that determines the color that should be
+ * used given an x and y coordinate using a quartered image pattern (the original image
+ * is scaled down and tiled over itself). You can create
+ * private helper functions inside this class, as well as local storage, if
+ * necessary. Remember to overload a destructor if you need to.
+ */
+class QuarterColorPicker : public ColorPicker {
 public:
     /**
      * Constructs a new QuarterColorPicker.
@@ -26,7 +25,7 @@ public:
      * @pre inputimg is the same image which will be painted
      * @pre inputimg width and height are both even
      */
-    QuarterColorPicker(PNG& inputimg, unsigned char b_amount);
+    QuarterColorPicker(PNG &inputimg, unsigned char b_amount);
 
     /**
      * Picks the color for pixel (x, y).
@@ -35,27 +34,26 @@ public:
      * and tile the smaller image in a 2 x 2 grid over the original dimensions
      * of the image, and return the pixel at the appropriate coordinate from
      * the tiled image, brightened on each R/G/B channel by the required amount.
-     * 
+     *
      * The value of each pixel in the scaled image will be a bilinear interpolation
      * of a 2x2 pixel region from the original image, with each of the R/G/B/A channels
      * processed individually. Interpolate over the x-axis before the y-axis.
-	 * Truncate any fractional values on the R/G/B channels. Brighten each color channel
+     * Truncate any fractional values on the R/G/B channels. Brighten each color channel
      * as the final step.
-     * 
+     *
      * @pre referenceimg width and height are both even
      */
     virtual RGBAPixel operator()(PixelPoint p);
 
 private:
-
-    PNG referenceimg;            // original image used in the flood fill algorithm
+    PNG referenceimg;           // original image used in the flood fill algorithm
     unsigned char brightamount; // amount to increase each R/G/B channel
 
-    /**
-     * @todo Define any additional private member variables or helper
-     *	functions in quarterColorPicker_private.h as you see fit.
-     */
-    #include "quarterColorPicker_private.h"
+/**
+ * @todo Define any additional private member variables or helper
+ *	functions in quarterColorPicker_private.h as you see fit.
+ */
+#include "quarterColorPicker_private.h"
 };
 
 #endif
